e9f4a98fc35202dd6c134ac4f3fa207c5f51ff54
Marcos Artimundo <51058488+martimundo@users.noreply.github.com>
2023-11-14 20:11:33 -0300

    Implmentado pacote dompdf

--- app/Exports/TarefasExport.php
+++ app/Exports/TarefasExport.php
@@ -5,8 +5,9 @@ namespace App\Exports;
 use App\Models\Tarefa;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class TarefasExport implements FromCollection, WithHeadings
+class TarefasExport implements FromCollection, WithHeadings, WithMapping
 {
     /**
      * @return \Illuminate\Support\Collection
@@ -17,9 +18,29 @@ class TarefasExport implements FromCollection, WithHeadings
         return auth()->user()->tarefas()->get();
     }
 
-    public function headings():array
+    public function headings(): array
     {
-        $heading = ['ID Tarefas', 'ID Usuário','Desc. Tarefa','Dt. Limite','Dt. Criação','Dt. Atualização' ];
+        $heading = [
+            'ID Tarefas',
+            'ID Usuário',
+            'Desc. Tarefa',
+            'Dt. Limite',
+            'Dt. Criação',
+            'Dt. Atualização'
+        ];
         return $heading;
+    }
+
+    public function map($linha): array
+    {
+
+        return[
+            $linha->id,
+            $linha->user_id,
+            $linha->tarefa,
+            date('d/m/Y', strtotime($linha->data_limite_conclusao)),
+            date('d/m/Y', strtotime($linha->created_at)),
+            date('d/m/Y', strtotime($linha->updated_at)),
+        ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Auth::routes(['verify'=>true]);
